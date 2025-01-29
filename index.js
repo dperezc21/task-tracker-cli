4a3d55a55b1f270944998aa22af18ed1c54216a3
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-01-28 19:48:23 -0500

    feat: use object data to add and update

--- index.js
+++ index.js
@@ -4,17 +4,27 @@ const {addTask, updateTask} = require("./crud-task");
 
 const arguments = process.argv.slice(2, process.argv.length);
 const action = arguments[0];
-const description = arguments[1];
+let description = arguments[1];
 
-const taskId = arguments[1];
-const desc = arguments[2];
+let taskId = "";
 
 const actionsAllowed = {
-    "add": (description) => addTask(description),
-    "update": (taskId, description) => updateTask(taskId, description),
+    "add": ({description}) => addTask(description),
+    "update": ({taskId, description}) => updateTask(taskId, description),
 }
 
 if(!(action in actionsAllowed)) throw new Error(`action ${action} invalid`);
 
-//actionsAllowed[action](description);
-actionsAllowed[action](taskId, desc);
+if(action === "add") {
+    description = arguments[1];
+} else if(action === "update" || action === "delete") {
+    taskId = arguments[1];
+    description = arguments[2];
+}
+
+const data = {
+    taskId,
+    description
+}
+
+actionsAllowed[action](data);
